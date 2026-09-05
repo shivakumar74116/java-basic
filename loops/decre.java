78c1e411e965137e28fb8d47c69badd9b921d7ae
Shivakumar <shivakumar74116@gmail.com>
2026-09-05 23:45:28 +0530

    decrement of two

---
+++ loops/decre.java
@@ -0,0 +1,29 @@
+package loops;
+public class decre {
+    public static void main(String[] args){
+
+        // for(int i = 100; i>=10; i= i-10){
+        //     System.out.println(i);
+        // }
+
+        //while
+
+        // int i = 100;
+        // while(i>=10){
+        //     System.out.println(i);
+        //     i= i-10;
+        // }
+
+        //DO - while
+
+        int i = 100;
+        do{
+            System.out.println(i);
+            i= i - 10;
+
+        }
+        while(i>=10); 
+        
+    }
+    
+}
---
+++ loops/decrepower.java
@@ -0,0 +1,16 @@
+package loops;
+
+class Decrepower {
+    public static void main(String[] args){
+        for(int i=64; i<=1; i=i/2)
+
+            System.out.println(i);
+
+            
+
+
+            
+
+    }
+    
+}

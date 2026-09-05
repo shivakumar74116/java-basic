393f9015725e6dfa3775a0ffbb8af3793cc5a06a
Shivakumar <shivakumar74116@gmail.com>
2026-09-05 22:00:43 +0530

    decrement

---
+++ loopss/decre.java
@@ -0,0 +1,13 @@
+package loopss;
+import java.util.*;
+public class decre {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter start number");
+        int start = sc.nextInt();
+
+        System.out.println("Enter second number");
+        int end = sc.nextInt()
+
+    }
+}

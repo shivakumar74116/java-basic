5525b0fe5bc1a3151494ec96dbdaf8540b8aa49f
Shivakumar <shivakumar74116@gmail.com>
2026-09-09 10:44:08 +0530

    OddNumber

---
+++ loopss/Q3_oddNumber.java
@@ -0,0 +1,14 @@
+package loopss;
+
+public class Q3_oddNumber {
+    public static void main(String[] args){
+        int i = 1;
+        while(i<=10){
+            System.out.println(i);
+            i = i+2;
+        }
+
+    
+    }
+    
+}

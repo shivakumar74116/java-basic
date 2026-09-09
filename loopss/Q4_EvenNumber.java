5edb88665f79762d8c8b315aaf10086d54f87545
Shivakumar <shivakumar74116@gmail.com>
2026-09-09 10:44:22 +0530

    EvenNumber

---
+++ loopss/Q4_EvenNumber.java
@@ -0,0 +1,12 @@
+package loopss;
+
+public class Q4_EvenNumber {
+    public static void main(String[] args){
+        int i = 0;
+        while(i<=20){
+            System.out.println(i);
+            i= i +2;
+        }
+    }
+    
+}
